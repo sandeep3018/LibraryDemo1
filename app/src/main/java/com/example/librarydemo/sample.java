8d9acd8358f7540316be80ffcd69d6559dd1d861
Mohammad asif <asif.devand153@gmail.com>
2020-02-24 11:31:01 +0530

    intial commit

---
+++ app/src/main/java/com/example/librarydemo/sample.java
@@ -0,0 +1,10 @@
+package com.example.librarydemo;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class sample {
+    public static void sampl(Context c, String message) {
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
+    }
+}
